e4ff14b8a8c838002d6e65f02021b8094e20a666
zli <zli@mail.com>
2024-12-17 17:46:11 +0100

    updated

--- experimental/llama_parameters.py
+++ experimental/llama_parameters.py
@@ -1,18 +1,36 @@
-def compute_total_parameters(embedding_dim: int,
-                             head_dim: int,
-                             num_key_value_heads: int,
-                             num_decoder_blocks: int,
-                             vocab_size: int) -> int:
+from transformers import PretrainedConfig
+
+
+def compute_total_parameters(config: PretrainedConfig, decoder_bias: bool) -> int:
+    """
+    Compute the total number of parameters in the model.
+    Args:
+        config: The configuration of the model.
+        decoder_bias: A boolean indicating whether to include the bias in the decoder blocks."""
+    vocab_size = config.vocab_size
+    hidden_size = config.hidden_size
+    num_hidden_layers = config.num_hidden_layers
+    num_key_value_heads = config.num_key_value_heads
+    num_attention_heads = config.num_attention_heads
+    inter_size = config.intermediate_size
+    head_dim = hidden_size // num_attention_heads
+
     # Embedding generation
-    embedding_params = vocab_size * embedding_dim
+    embedding_params = vocab_size * hidden_size
 
     # Decoder blocks (self-attention + FFN)
-    decoder_blocks_params = (embedding_dim + 2 * embedding_dim * embedding_dim + 2 * embedding_dim * num_key_value_heads * head_dim +
-                                 embedding_dim + 12 * embedding_dim * embedding_dim) * num_decoder_blocks
+    decoder_blocks_params = (hidden_size +
+                             2*hidden_size*hidden_size + 2*hidden_size*num_key_value_heads*head_dim +
+                             hidden_size +
+                             3*inter_size*hidden_size) * num_hidden_layers
+
+    if decoder_bias:
+        decoder_blocks_params += num_hidden_layers * (hidden_size + 2 * num_key_value_heads*head_dim)
+
     # RMS norm
-    rms_norm_params = embedding_dim
+    rms_norm_params = hidden_size
 
     # LM head
-    lm_head_params = vocab_size * embedding_dim
+    lm_head_params = vocab_size * hidden_size
 
     return embedding_params + decoder_blocks_params + rms_norm_params + lm_head_params
--- experimental/qwen_study.py
+++ experimental/qwen_study.py
@@ -1,12 +1,14 @@
-from llama_parameters import compute_total_parameters
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, Qwen2Config
 
-model_id = "meta-llama/Llama-3.2-1B-Instruct"
+model_id = "Qwen/Qwen2.5-1.5B-Instruct"
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side='left')
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
+
+config = AutoConfig.from_pretrained(model_id)
+
 model = AutoModelForCausalLM.from_pretrained(model_id, device_map='auto', torch_dtype="auto")
 model.eval()
 
@@ -16,15 +18,17 @@ for name, p in model.state_dict().items():
     accum += p.numel()
     print(f"{name}: {list(p.shape)} {p.numel()}\tAccumulated: {accum}")
 
-assert compute_total_parameters(config=model.config, decoder_bias=False) == 1498482688
+from llama_parameters import compute_total_parameters
+assert compute_total_parameters(config, True) == 1777088000
 
 messages = [[{"role": "user", "content": "2+3=? Answer with a single number."}],
-            [{"role": "user", "content": "Hi"}]]
-model_inputs = tokenizer.apply_chat_template(
-    messages, add_generation_prompt=True, return_tensors='pt', padding=True).to(device)
+    [{"role": "user", "content": "Hi"}]]
+model_inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors='pt', padding=True).to(device)
 generate_kwargs = {'input_ids': model_inputs,
                    'do_sample': False}
 outputs = model(**generate_kwargs)
 
-token_ids = outputs.logits[:, -1:].argmax(dim=-1)
+token_ids = outputs.logits[:,-1:].argmax(dim=-1)
 print(tokenizer.batch_decode(token_ids, skip_special_tokens=True))
+
+
